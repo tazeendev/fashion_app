6a4a3871e156a5a1e913c462dab55ef43533ab93
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-07-27 12:00:36 -0700

    hahahha

---
+++ lib/core/utils/app_images/app_images.dart
@@ -0,0 +1,13 @@
+class AppImages{
+  static const String onboarding1 = 'assets/images/onboarding1.jpg';
+  static const String onboarding2 = 'assets/images/onboarding2.jpg';
+  static const String onboarding3 = 'assets/images/onboarding3.jpg';
+  static const String product1 = 'assets/images/product1.jpg';
+  static const String product2 = 'assets/images/product2.jpg';
+  static const String product3 = 'assets/images/product3.jpg';
+  static const String product4 = 'assets/images/product4.jpg';
+  static const String product5 = 'assets/images/product5.jpg';
+  static const String product6 = 'assets/images/product6.jpg';
+  static const String product7 = 'assets/images/product7.jpg';
+  static const String welcome = 'assets/images/welcome.jpg';
+}
