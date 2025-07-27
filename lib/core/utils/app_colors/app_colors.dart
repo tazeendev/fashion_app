af9c220064be142927015a8a366a659a8af36f8f
tazeen-dev <tazeenzahrabatool@gmail.com>
2025-07-27 11:08:17 -0700

    hello

---
+++ lib/core/utils/app_colors/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class AppColors{
+  static const Color blackColor=Color(0xff212529);
+  static const Color whiteColor=Color(0xfff8f9fa);
+  static const Color blueColor=Color(0xff3f37c9);
+  static const Color purpleColor=Color(0xff240046);
+  static const Color redColor=Color(0xffb23a48);
+  static const Color meronColor=Color(0xff8c2f39);
+  static const Color skyblueColor=Color(0xff81c3d7);
+  static const Color greyColor=Color(0xff8d99ae);
+  static const Color brownColor=Color(0xffc08552);
+  static const Color yelloColor=Color(0xfff6bd60);
+
+}
